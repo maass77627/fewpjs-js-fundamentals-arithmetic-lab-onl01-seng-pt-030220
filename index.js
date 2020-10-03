2ac610bc2002860d064b8877e245edc96df195a0
kelly r maass <kmaass77627@gmail.com>
2020-10-03 01:38:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+// function multiply() {
+//   31*2
+// }
+
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.random();
+
+let mod = 19 % 5
+
+let max = Math.max(5, 4, 20)
